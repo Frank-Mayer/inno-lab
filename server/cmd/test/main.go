c5afca12bfbc5940afbb22fff427454451c7372c
Frank Mayer <gh@frankmayer.dev>
2024-01-27 21:33:41 +0100

    url test

--- server/cmd/test/main.go
+++ server/cmd/test/main.go
@@ -7,8 +7,11 @@ import (
 
 func main() {
 	// type something
-	keyboard.SendKeys("/Hello World!")
+	// keyboard.SendKeys("/Hello World!")
+	str := `https://firebasestorage.googleapis.com/v0/b/inno-lab-85f72.appspot.com/o/1706386572153939064.jpg?alt=media&token=747dc81e-6f2c-440f-b811-5502af2c094b`
+	println(str)
+	keyboard.SendKeys(str)
 
 	// move mouse
-	robotgo.MoveSmooth(100, 200, 1.0, 100.0)
+	robotgo.MoveSmooth(100, 200, 1.0, 3.0)
 }
