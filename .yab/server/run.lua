5372da666da4bd8eee29daaf7e81584b5f3f0c57
Frank Mayer <gh@frankmayer.dev>
2024-02-10 14:08:46 +0100

    auto credentials location

--- .yab/server/run.lua
+++ .yab/server/run.lua
@@ -1,7 +1,8 @@
 require("env")
 local yab = require("yab")
-yab.setenv("CREDENTIALS_LOCATION","/Users/Shared/veritas/serviceAccountKey.json")
+
+yab.setenv("CREDENTIALS_LOCATION", yab.stdout("pwd") .. "/serviceAccountKey.json")
 yab.setenv("FULLSCREEN", "true")
 yab.cd("server", function()
-    os.execute("go run ./cmd/server")
+	os.execute("go run ./cmd/server")
 end)
